963aed0ec2adad32c245232b4493bab7768b0964
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-11-24 23:40:53 -0300

    Update codewars-C.cs

--- codewars-C.cs
+++ codewars-C.cs
@@ -7,3 +7,14 @@ public class Kata
     return "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9];
   }
 }
+
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    int result = Convert.ToInt32(Math.Floor(time*0.5));
+    return result;
+  }
+}
