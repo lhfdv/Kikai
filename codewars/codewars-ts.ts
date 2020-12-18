3ed7a06cf6afdf32629c49b0986fce439a4bf86c
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-12-17 23:39:12 -0300

    Update codewars-ts.ts

--- codewars/codewars-ts.ts
+++ codewars/codewars-ts.ts
@@ -7,3 +7,10 @@ export function unluckyDays(year: number): number {
   }
   return res;
 }
+
+=========================================================
+
+export function getSum(a: number, b: number): number {
+  let min = Math.min(a, b), max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
