c8b2448c2f60699c95dca9f9695911b7c7b5dc62
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-12-22 07:31:41 -0300

    Update codewars-ts.ts

--- codewars/codewars-ts.ts
+++ codewars/codewars-ts.ts
@@ -14,3 +14,11 @@ export function getSum(a: number, b: number): number {
   let min = Math.min(a, b), max = Math.max(a, b);
   return (max - min + 1) * (min + max) / 2;
 }
+
+=========================================================
+
+export class Kata {
+  static getCount(str: string) {
+    return (str.match(/[aeiou]/g) || []).length;
+  }
+}
